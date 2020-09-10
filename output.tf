554e5954521d2d07bdf0121a08474ed1056d159e
Pinkesh Valdria <pvaldria@yahoo.com>
2020-09-10 05:12:04 -0700

    ORM changes

--- output.tf
+++ output.tf
@@ -18,11 +18,11 @@ END
 output "SSH-login" {
 value = <<END
 
-        Bastion: ssh -i /Users/pvaldria/.ssh/gg_id_rsa ${var.ssh_user}@${element(concat(oci_core_instance.bastion.*.public_ip, [""]), 0)}
+        Bastion: ssh -i CHANGEME ${var.ssh_user}@${element(concat(oci_core_instance.bastion.*.public_ip, [""]), 0)}
 
-        Storage Server-1: ssh -i /Users/pvaldria/.ssh/gg_id_rsa  -o BatchMode=yes -o StrictHostkeyChecking=no  -o ProxyCommand="ssh -i /Users/pvaldria/.ssh/gg_id_rsa -o BatchMode=yes -o StrictHostkeyChecking=no ${var.ssh_user}@${element(concat(oci_core_instance.bastion.*.public_ip, [""]), 0)} -W %h:%p %r" ${var.ssh_user}@${element(concat(oci_core_instance.storage_server.*.private_ip, [""]), 0)}
+        Storage Server-1: ssh -i CHANGEME  -o BatchMode=yes -o StrictHostkeyChecking=no  -o ProxyCommand="ssh -i CHANGEME -o BatchMode=yes -o StrictHostkeyChecking=no ${var.ssh_user}@${element(concat(oci_core_instance.bastion.*.public_ip, [""]), 0)} -W %h:%p %r" ${var.ssh_user}@${element(concat(oci_core_instance.storage_server.*.private_ip, [""]), 0)}
 
-        Client-1: ssh -i /Users/pvaldria/.ssh/gg_id_rsa  -o BatchMode=yes -o StrictHostkeyChecking=no  -o ProxyCommand="ssh -i /Users/pvaldria/.ssh/gg_id_rsa -o BatchMode=yes -o StrictHostkeyChecking=no ${var.ssh_user}@${element(concat(oci_core_instance.bastion.*.public_ip, [""]), 0)} -W %h:%p %r" ${var.ssh_user}@${element(concat(oci_core_instance.client_node.*.private_ip, [""]), 0)}
+        Client-1: ssh -i CHANGEME  -o BatchMode=yes -o StrictHostkeyChecking=no  -o ProxyCommand="ssh -i CHANGEME -o BatchMode=yes -o StrictHostkeyChecking=no ${var.ssh_user}@${element(concat(oci_core_instance.bastion.*.public_ip, [""]), 0)} -W %h:%p %r" ${var.ssh_user}@${element(concat(oci_core_instance.client_node.*.private_ip, [""]), 0)}
 
 END
 }
