64a4ed54d46ff9a8a3f173748b87bb5e020e3ca9
anoopks1207 <anoopks1207@gmail.com>
2023-02-14 19:52:09 +0530

    variables update

--- provider.tf
+++ provider.tf
@@ -1,22 +1,22 @@
 
-provider "oci" {
-  version          = ">= 3.94.0"
-  tenancy_ocid     = var.tenancy_ocid
-  user_ocid        = var.user_ocid
-  fingerprint      = var.fingerprint
-  private_key_path = var.private_key_path
-  region           = var.region
+#provider "oci" {
+#  version          = ">= 3.94.0"
+#  tenancy_ocid     = var.tenancy_ocid
+#  user_ocid        = var.user_ocid
+#  fingerprint      = var.fingerprint
+#  private_key_path = var.private_key_path
+#  region           = var.region
 }
 
 # Variables required by the OCI Provider only when running Terraform CLI with standard user based Authentication
-variable "user_ocid" {
-}
+#variable "user_ocid" {
+#}
 
-variable "fingerprint" {
-}
+#variable "fingerprint" {
+#}
 
-variable "private_key_path" {
-}
+#variable "private_key_path" {
+#}
 
-variable "ssh_private_key" {
-}
+#variable "ssh_private_key" {
+#}
